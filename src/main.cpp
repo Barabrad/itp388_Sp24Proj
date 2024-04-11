62426711483bd19422495093fb35e6f6d90eb2c9
Brad Barakat <78241382+Barabrad@users.noreply.github.com>
2024-04-10 17:36:36 -0700

    Placeholders and Organizing
    I put placeholders to get rid of error messages, that way the code could be validated.
    I also did a little organizing and standardization of formatting for constant variables.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #define BLYNK_TEMPLATE_ID "[REDACTED]"
 #define BLYNK_TEMPLATE_NAME "388 Project"
 #define BLYNK_AUTH_TOKEN "[REDACTED]"
+#define RFID_ADDR 0x7D // Default I2C address for RFID
 
 #include <Arduino.h>
 #include <WiFi.h>
@@ -10,16 +11,22 @@
 #include <Wire.h> 
 #include "SparkFun_Qwiic_Rfid.h"
 
+typedef unsigned long u_long;
+
 // Your WiFi credentials.
 // Set password to "" for open networks.
 // https://examples.blynk.cc/?board=ESP32&shield=ESP32%20WiFi&example=GettingStarted%2FBlynkBlink
 char ssid[] = "YourNetworkName";
 char pass[] = "YourPassword";
 
+// Initialize RFID reader
+Qwiic_Rfid myRfid(RFID_ADDR);
+
+// ***************************** //
+// ***** Constant Globals ****** //
+// ***************************** //
 // Pins
 const int8_t BUZZER_PIN = 14;
-
-// Constants
 // Array for notes (zeros are rests)
 const uint8_t BPM = 135; // Beats per minute
 const float BPS = BPM/60.0; // Beats per second
@@ -32,29 +39,27 @@ const float MELODY_BEATS[MELODY_LEN] = {0.25, 0.25, 0.25, 0.25, 0.75, 0.75, 1, 0
 const uint8_t TRITONE_LEN = 2; // # of notes (including rests)
 const uint16_t TRITONE[TRITONE_LEN] = {NOTE_F5, NOTE_B4};
 const float TRITONE_BEATS[TRITONE_LEN] = {0.5, 0.5};
-//Timing
-const int openTime = 15000; // 15 seconds
-const int scanDelay = 5000; // 5 seconds
-
-// Non-constant variables
+// Accepted RFID Cards
+const String VALID_IDS[] = {"Enter IDs here after you test each card", "The ways in which you talk to me", "Suguru", "Have me wishing I were gone", "Satoru"};
 // Timing
-unsigned long prevMillisDoor = 0;
-unsigned long prevMillisScan = 0;
-//unsigned long currMillis = 0;
+const u_long TIME_OPEN = 15000; // 15 seconds
+const u_long SCAN_DELAY = 5000; // 5 seconds
+
+// ***************************** //
+// **** Non-Const. Globals ***** //
+// ***************************** //
+// Timing
+u_long prevMillisDoor = 0;
+u_long prevMillisScan = 0;
+//u_long currMillis = 0;
 // Controls
 bool doorOpen = false;
 //bool justCheckedRFID = false;
 
-// put function declarations here:
-void playSongWithDelay(const uint16_t*, const float*, const uint8_t);
-void grantAccess();
-void denyAccess();
-void open();
-void close();
-bool verifyRFID();
-
-// Helper functions
-void playSongWithDelay(const int* melody, const float* beats, const int len) {
+// ***************************** //
+// ***** Helper Functions ****** //
+// ***************************** //
+void playSongWithDelay(const uint16_t* melody, const float* beats, const uint8_t len) {
   // Just for now, we're going to use delay()
   // To calculate the note time, we will use the beats/sec and the number of beats for each note
   for (int i = 0; i < len; i++) {
@@ -96,42 +101,55 @@ void close() {
   Serial.println("Closing door...");
 }
 
-bool verifyRFID() {
-  // Verify RFID card
-  if (/*RFID is valid*/) {
-    return true;
-  } else {
-    return false;
-  }
+bool isTilted() {
+  // Figure out how to read from tilt sensor
+  return false; // Placeholder so we can verify code works
 }
 
+bool verifyRFID(String tag) {
+  // Verify RFID card
+  bool isValid = false;
+  for (String s : VALID_IDS) {
+    if (tag == s) {isValid = true; break;} // If the tag matches a valid ID, break with true
+  }
+  return isValid;
+}
+
+// ***************************** //
+// ****** Main Functions ******* //
+// ***************************** //
 void setup() {
-  // put your setup code here, to run once:
+  // Begin Serial
   Serial.begin(9600);
-  Wire.begin();
+  // Begin I-squared-C
+	Wire.begin(); 
+  // Begin Blynk
   Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+  // Make sure RFID is reachable
+  if (!(myRfid.begin())) {Serial.println("Could not communicate with Qwiic RFID!");}
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   Blynk.run();
-  if (/*RFID is detected */ & (millis() - prevMillisScan > scanDelay)) {
+  String tag = myRfid.getTag(); // Check for a scan
+  if ((tag != "") && (millis() - prevMillisScan > SCAN_DELAY)) {
     prevMillisScan = millis();
     Serial.println("RFID detected. Verifying...");
-    if (verifyRFID()) {
+    if (verifyRFID(tag)) {
       grantAccess();
       open();
-      continue;
+      //continue; // I don't think "continue;" works in loop()
     } else {
       denyAccess();
-      continue;
+      //continue;
     }
   }
-  if (/*tilt sensor tilted*/) {
+  // We need to figure out how to prevent the denial of access temporarily after a successful swipe
+  if (isTilted()) {
     denyAccess();
     close();
   }
-  if (doorOpen & (millis() - prevMillisDoor > openTime)) {
+  if (doorOpen && (millis() - prevMillisDoor > TIME_OPEN)) {
     close();
   }
 }
